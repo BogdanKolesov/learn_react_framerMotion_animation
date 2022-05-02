66cc257133e0712e567070b328887d58b5c140ce
Bogdan <bogdan_kolesov@bk.ru>
2022-05-02 12:23:39 +0600

    variants

--- src/lessons/LessonOne.js
+++ src/lessons/LessonOne.js
@@ -3,28 +3,50 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 const LessonOne = () => {
+    const pVariants = {
+        hidden: {
+            x: -1000,
+            opacity: 0
+        },
+        visible: {
+            x: 0,
+            opacity: 1
+        }
+    }
     return (
-        <motion.div
-            style={{
-                width: 140,
-                height: 140
-            }}
-            animate={{
-                rotate: 360
-            }}
-            // initial={{}}
-            transition={{
-                delay: 1,
-                duration: 2,
-                repeat: Infinity,
-                repeatDelay: 2,
-                repeatType: 'reverse',
-                type: 'spring',
-                ease: 'easeInOut'
-            }}
-        >
-            <Square />
-        </motion.div>
+        <>
+            <motion.div
+                style={{
+                    width: 140,
+                    height: 140
+                }}
+                animate={{
+                    rotate: 360
+                }}
+                // initial={{}}
+                transition={{
+                    delay: 1,
+                    duration: 2,
+                    repeat: Infinity,
+                    repeatDelay: 2,
+                    repeatType: 'reverse',
+                    type: 'spring',
+                    ease: 'easeInOut'
+                }}
+            >
+                <Square />
+            </motion.div>
+            <motion.p
+                initial={'hidden'}
+                animate={'visible'}
+                transition={{
+                    delay: 1
+                }}
+                variants={pVariants}
+            >
+                Some Text
+            </motion.p>
+        </>
     )
 }
 
